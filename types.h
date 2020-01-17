730dd7389042d3a15c71e772e75d5956e75fd60b
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-17 04:51:02 -0500

    Create types.h

---
+++ types.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H_INCLUDED
+#define TYPES_H_INCLUDED
+
+enum { EMPTY, rP, rN, rB, rR, rQ, rQ, bP, bN, bB, bR, bQ, bQ, yP, yN, yB, yR, yQ, yQ, gP, gN, gB, gR, gQ, gQ };
+
+enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_I, FILE_J, FILE_K, FILE_L, FILE_M, FILE_N, FILE_NONE }
+enum { RANK_A, RANK_B, RANK_C, RANK_D, RANK_E, RANK_F, RANK_G, RANK_H, RANK_I, RANK_J, RANK_K, RANK_L, RANK_M, RANK_N, RANK_NONE }
+
+enum { FALSE, TRUE }
+
+#endif
